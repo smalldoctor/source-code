dbc11f0fcfed19df2db668a18355d61b514ef117
thinkings <15312408287@163.com>
2017-11-08 21:49:15 +0800

    Connector学习

--- tomcat-7.x/java/org/apache/coyote/ProtocolHandler.java
+++ tomcat-7.x/java/org/apache/coyote/ProtocolHandler.java
@@ -1,7 +1,36 @@
 package org.apache.coyote;
 
+import java.util.concurrent.Executor;
+
 /**
- * 对Protocol的能力的抽象；
+ * 对Protocol的通用能力的抽象；
  */
 public interface ProtocolHandler {
+    public void getAdapter();
+
+    public void setAdapter(Adapter adapter);
+
+    /**
+     * 协议使用的线程池
+     *
+     * @return
+     */
+    public Executor getExecutor();
+
+    public void init() throws Exception;
+
+    public void start() throws Exception;
+
+    public void pause() throws Exception;
+
+    public void resume() throws Exception;
+
+    public void stop() throws Exception;
+
+    public void destroy() throws Exception;
+
+    /**
+     * 使用本地APR库
+     */
+    public void isAprRequired();
 }
