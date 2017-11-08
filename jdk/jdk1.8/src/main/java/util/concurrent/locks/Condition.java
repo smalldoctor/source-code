79171456dd8d50c45281d631958323222bf784f4
thinkings <15312408287@163.com>
2017-11-08 10:36:18 +0800

    Condition及await/notify学习

--- jdk/jdk1.8/src/main/java/util/concurrent/locks/Condition.java
+++ jdk/jdk1.8/src/main/java/util/concurrent/locks/Condition.java
@@ -5,7 +5,9 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * wait-sets的实现包括monitor object和Synchronize；
- * Lock的是Synchronize的替代实现；Condition就是monitor object的替代实现
+ * Lock的是Synchronize的替代实现；Condition就是monitor object的替代实现；
+ * <p>
+ * 整个是wait/notify的重写；
  * <p>
  * 如生产者和消费者的中间缓存，当缓存有数据时消费数据，当缓存无数据时停止消费，等待数据生产；
  * 当缓存无数据时生产数据，当缓存数据满时停止生产等待数据消费；
@@ -59,13 +61,60 @@ public interface Condition {
      * 2. 其他线程调用Condition SignalAll
      * 3. 其他线程调用 {@link Thread#interrupt()}中断当前线程，当前线程支持中断
      * 4. 虚假唤醒
+     * <p>
+     * 在线程从await方法返回之前，需要先获取锁。一旦线程返回，则说明已经持有锁；
+     * <p>
+     * 在进入方法之前，线程中断标志已经设置或者在等待过程中发生中断，则抛出中断异常；
+     * 并且中断标志被清楚；
+     * <p>
+     * 对中断异常的场景，需要保证signal被转发给下个等待的线程，避免无限等待；
      *
      * @throws InterruptedException
      */
     void await() throws InterruptedException;
 
+    /**
+     * 当前线程挂起进入等待，直到被SIGNAL或者发生中断；
+     * 当前线程会处于等待休眠状态(lie dormant)，发生以下场景会被唤醒：
+     * 1. 其他线程调用Condition（与await相同的Condition对象） SIGNAL（随机唤醒任意一个
+     * 等待的线程），同时正好唤醒是当前的线程；
+     * 2. 其他线程调用Condition SignalAll
+     * 3. 虚假唤醒
+     * <p>
+     * 不考虑中断的场景，即使发生中断，也等待signal;但是会设置中断标志
+     */
     void awaitUninterruptibly();
 
+    /**
+     * 当前线程挂起进入等待，直到被SIGNAL或者发生中断；
+     * 当前线程会处于等待休眠状态(lie dormant)，发生以下场景会被唤醒：
+     * 1. 其他线程调用Condition（与await相同的Condition对象） SIGNAL（随机唤醒任意一个
+     * 等待的线程），同时正好唤醒是当前的线程；
+     * 2. 其他线程调用Condition SignalAll
+     * 3. 其他线程调用 {@link Thread#interrupt()}中断当前线程，当前线程支持中断
+     * 4. 虚假唤醒
+     * 5. 超时时间到达
+     * <p>
+     * *  <pre> {@code
+     * boolean aMethod(long timeout, TimeUnit unit) {
+     *   long nanos = unit.toNanos(timeout);
+     *   lock.lock();
+     *   try {
+     *     while (!conditionBeingWaitedFor()) {
+     *       if (nanos <= 0L)
+     *         return false;
+     *       nanos = theCondition.awaitNanos(nanos);
+     *     }
+     *     // ...
+     *   } finally {
+     *     lock.unlock();
+     *   }
+     * }}</pre>
+     *
+     * @param nanosTimeout
+     * @return
+     * @throws InterruptedException
+     */
     long awaitNanos(long nanosTimeout) throws InterruptedException;
 
     boolean await(long time, TimeUnit unit) throws InterruptedException;
