ada580bd6a0b2e0d6c784c7571090f2a13316658
thinkings <15312408287@163.com>
2017-11-16 23:12:25 +0800

    Connector学习

---
+++ tomcat-7.x/java/org/apache/tomcat/jni/Library.java
@@ -0,0 +1,10 @@
+package org.apache.tomcat.jni;
+
+/**
+ * JNI 本地库工具类
+ */
+public final class Library {
+    //-------------------------------------------------  Static Variables
+    // 本地动态链接库的名字
+    private static final String[] NAMES = {"tcnative-1", "libtcnative-1"};
+}
