8990bb07f2ce4b8e55a4bf4a7843361f0543fdc4
thinkings <15312408287@163.com>
2017-11-26 21:53:10 +0800

    Common学习

---
+++ dubbo/dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/ExtensionLoader.java
@@ -0,0 +1,18 @@
+package com.alibaba.dubbo.common.extension;
+
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
+
+/**
+ * Dubbo的扩展点获取的工具类；
+ * 1. 自动注入关联扩展点
+ * 2. 自动Wrap扩展点
+ * 3. 默认获取到是Adaptive Instance,Adaptive Instance 根据Adapter注解的配置获取
+ * 指定的扩展实现；
+ */
+public class ExtensionLoader {
+    //-------------------------------------------------  Static Variables
+    private static final Logger logger = LoggerFactory.getLogger(ExtensionLoader.class);
+
+
+}
