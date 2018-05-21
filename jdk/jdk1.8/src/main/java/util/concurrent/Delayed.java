0fad57f4ae68e65886662feafe3f00cc635b868c
thinkings <15312408287@163.com>
2018-05-21 15:01:08 +0800

    并发包学习

--- jdk/jdk1.8/src/main/java/util/concurrent/Delayed.java
+++ jdk/jdk1.8/src/main/java/util/concurrent/Delayed.java
@@ -8,4 +8,15 @@ package java.util.concurrent;
  * Delayed用于表示这个对象必须延迟执行；
  */
 public interface Delayed extends Comparable<Delayed> {
+
+    /**
+     * Returns the remaining delay associated with this object, in the
+     * given time unit.
+     *
+     * @param unit the time unit
+     * @return the remaining delay; zero or negative values indicate
+     * that the delay has already elapsed
+     */
+    long getDelay(TimeUnit unit);
 }
+
